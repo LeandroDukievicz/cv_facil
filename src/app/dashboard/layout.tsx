494dce57d494dafd63fe832fc7f5ab2722756d9c
LeandroDukievicz <ldukie@icloud.com>
2024-11-26 17:28:21 -0300

    📦 FEAT: add components in sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
-import Logo from "@/assets/logo.svg";
+import Logo from "@/app/assets/logo.svg";
+import { NavItems } from "@/components/dashboard/nav-items";
 
 type DashboardLayoutProps = {
   children: ReactNode;
@@ -9,7 +10,10 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="w-full h-screen overflow-hidden grid grid-cols-[300px,1fer]">
       <aside className="w-full h-full flex flex-col items-center border-r border-muted">
-        <div className="w-full p-6 border-b border-muted"></div>
+        <div className="w-full p-6 border-b border-muted">
+          <Logo className="max-w-[60px] mx-auto" />
+        </div>
+        <NavItems />
       </aside>
       <main>{children}</main>
     </div>
